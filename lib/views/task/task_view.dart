f24ee2870be506d859c422f1288a2966f5d4ab8d
da578 <dzulkiflianwar2@gmail.com>
2025-04-22 20:40:55 +0700

    refactor(task_view): size adjustments to icons and texts

--- lib/views/task/task_view.dart
+++ lib/views/task/task_view.dart
@@ -198,11 +198,13 @@ class _TaskViewState extends State<TaskView>
           Icon(
             Icons.do_disturb_alt_outlined,
             color: ThemeValues(context).colorScheme.onSurface,
+            size: 40,
           ),
           const SizedBox(height: 10),
           MyText(
             'No tasks available.',
             color: ThemeValues(context).colorScheme.onSurface,
+            size: 20,
           ),
         ],
       ),
@@ -218,11 +220,13 @@ class _TaskViewState extends State<TaskView>
           Icon(
             Icons.search_off_rounded,
             color: ThemeValues(context).colorScheme.onSurface,
+            size: 40,
           ),
           const SizedBox(height: 10),
           MyText(
             'No tasks found.',
             color: ThemeValues(context).colorScheme.onSurface,
+            size: 20,
           ),
         ],
       ),
