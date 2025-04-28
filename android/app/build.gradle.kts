da89f84eb66b4ce75853470c576920ef36f3258e
da578 <dzulkiflianwar2@gmail.com>
2025-04-28 07:24:33 +0700

    refactor: rename package name to com.dzul.todayly

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.dzul.todolist"
+    namespace = "com.dzul.todayly"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "27.0.12077973"
 
@@ -22,7 +22,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.dzul.todolist"
+        applicationId = "com.dzul.todayly"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
